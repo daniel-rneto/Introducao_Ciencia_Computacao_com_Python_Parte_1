9a470f43ebf819a825b2033e58752424c91b9d7b
Daniel <daniel.rneto@gmail.com>
2022-09-20 09:12:06 -0300

    Enunciado do exercicio adicionado

--- semana-4/lista_exercicio_semana_04/exercicio02.py
+++ semana-4/lista_exercicio_semana_04/exercicio02.py
@@ -1,3 +1,15 @@
+
+# Exercício 2
+# Receba um número inteiro positivo na entrada e imprima os  n n primeiros números ímpares naturais. Para a saída, siga o formato do exemplo abaixo.
+
+# Exemplo:
+# Digite o valor de n: 5
+# 1
+# 3
+# 5
+# 7
+# 9
+
 numeroIteracoes = int(input("Digite o valor de n: "))
 contador = 1
 
