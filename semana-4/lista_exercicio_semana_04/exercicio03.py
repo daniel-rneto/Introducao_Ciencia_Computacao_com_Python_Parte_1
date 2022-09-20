dcf1c1ef734d08ea7dd68ee157fb114be7cea24a
Daniel <daniel.rneto@gmail.com>
2022-09-20 09:15:10 -0300

    Adicionado enunciado do exercicio

--- semana-4/lista_exercicio_semana_04/exercicio03.py
+++ semana-4/lista_exercicio_semana_04/exercicio03.py
@@ -1,3 +1,14 @@
+# Exercício 3
+# Escreva um programa que receba um número inteiro na entrada, calcule e imprima a soma dos dígitos deste número na saída
+
+# Exemplo:
+# Digite um número inteiro: 123
+# 6
+
+# Dica: Para separar os dígitos, lembre-se: o operador "//" faz uma divisão inteira jogando fora o resto, 
+# ou seja, aquilo que é menor que o divisor; O operador "%" devolve apenas o resto da divisão inteira 
+# jogando fora o resultado, ou seja, tudo que é maior ou igual ao divisor.
+
 numero = int(input("Digite o valor de n: "))
 soma = 0
 
