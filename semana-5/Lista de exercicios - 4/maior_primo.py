2bb8b01d7c6874c9b39003d3823fde557b027dd6
Daniel <daniel.rneto@gmail.com>
2022-09-21 14:00:42 -0300

    alteração do nome do arquivo e inclusão de logica

---
+++ semana-5/Lista de exercicios - 4/maior_primo.py
@@ -0,0 +1,30 @@
+﻿# Exercício 2 - Primos
+# Escreva a função maior_primo que recebe um número inteiro maior ou igual a 2 
+# como parâmetro e devolve o maior número primo menor ou igual ao número passado à função
+
+# Exemplos de execução no shell do Python:
+
+# >>> maior_primo(100)
+# 97
+# >>> maior_primo(7)
+# 7
+
+def ePrimo(x):
+    maior_primo = 0
+    contador = 0
+    while (contador < x):
+        
+        if (contador % 2 != 0):
+            maior_primo = contador
+            #print (contador, " Nâo é Primo")
+        contador = contador + 1
+
+    return maior_primo
+
+def maior_primo(n):
+    #<Código do maior_primo>
+    return n
+
+
+y = int(input("digite um número: "))
+print ("maior primo:", ePrimo (y))
