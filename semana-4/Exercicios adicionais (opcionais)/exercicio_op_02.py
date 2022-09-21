7504ab109f7e126607dc18c5ffbdac72a9828b75
Daniel <daniel.rneto@gmail.com>
2022-09-21 09:40:17 -0300

    Enunciado adicionado

--- semana-4/Exercicios adicionais (opcionais)/exercicio_op_02.py
+++ semana-4/Exercicios adicionais (opcionais)/exercicio_op_02.py
@@ -1,3 +1,14 @@
+# Exercício 2 - Desafio do vídeo "Repetição com while"
+# Escreva um programa que receba um número inteiro na entrada e verifique se o número recebido possui ao menos um dígito com um dígito adjacente igual a ele. Caso exista, imprima "sim"; se não existir, imprima "não".
+
+# Exemplos:
+
+# Digite um número inteiro: 12345
+# não
+
+# Digite um número inteiro: 1011
+# sim
+
 numero = int(input("Digite um número inteiro: "))
 
 atual = -1
@@ -12,10 +23,7 @@ while (numero != 0):
         resp = True
         numero = 0
    
-    #print ("Atual:", atual)
-    #print ("Anterior:", anterior)
-    #print ("numero:", numero)
-    #print ("resp:",resp)
+  
     anterior = atual
 
 if (resp):
