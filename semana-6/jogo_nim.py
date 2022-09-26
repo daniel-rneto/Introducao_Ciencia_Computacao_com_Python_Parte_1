6f2d03fcea0a376182bd243099482f6a9eb63ba8
Daniel <daniel.rneto@gmail.com>
2022-09-26 10:09:16 -0300

    Adicionado chamada a funcao  de jogada do usuario e do computador

--- semana-6/jogo_nim.py
+++ semana-6/jogo_nim.py
@@ -1,3 +1,36 @@
 ﻿def computador_escolhe_jogada (n, m):
     return True
 
+def usuario_escolhe_jogada (n, m):
+    return True
+
+def partida ():
+    print ("Bem-vindo ao jogo do NIM! Escolha:")
+    print ("")
+    print ("1 - para jogar uma partida isolada")
+    print ("2 - para jogar um campeonato 2")
+    print ("")
+    resposta  = int(input())
+
+    if (resposta == 1):
+        print ("Voce escolheu partida isolada")
+    elif (resposta == 2):
+        print ("Voce escolheu um campeonato!")
+        print ()
+        print ("**** Rodada 1 ****")
+        print ()
+        qtde_pecas = int(input("Quantas peças? "))
+        limite_pecas_jogada= int(input("Limite de peças por jogada? "))
+        if (((limite_pecas_jogada + 1) % qtde_pecas) == 0):
+            print ("Você começa!")
+            usuario_escolhe_jogada (qtde_pecas, limite_pecas_jogada)
+            
+        else:
+            print ("Computador começa!")
+            computador_escolhe_jogada (qtde_pecas, limite_pecas_jogada)
+
+    else:
+        print ("Escolha errada. Tente novamente.")
+
+
+partida()
