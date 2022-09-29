d52d387dff6982a086daf32c175ef4acd45026c6
Daniel <daniel.rneto@gmail.com>
2022-09-29 13:23:09 -0300

    Exercicio - repetição encaixada com o calculo do fatorial em uma função

---
+++ semana-7/01-Exercicio Resolvido - Repetições Encaixadas/exercicio_refatorado_01.py
@@ -0,0 +1,17 @@
+﻿# Mesmo exercício anterior, só que o calculo do fatorial fica em uma funcao a parte
+
+def fatorial (x):
+    fatorial = 1
+    while (x > 1):
+        fatorial = fatorial * x
+        x = x - 1
+    return fatorial
+
+
+def main():
+    n = int(input("Digite um número inteiro: "))
+    while (n >= 0):
+        print (fatorial (n))
+        n = int(input("Digite um número inteiro: "))
+
+main()
