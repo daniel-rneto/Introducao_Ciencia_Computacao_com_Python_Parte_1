6980120c4bffbe58645c0afafb544d7f191a9b21
Daniel <daniel.rneto@gmail.com>
2022-09-29 11:13:24 -0300

    exercicio prova

---
+++ semana-7/Teste -Repetições Encaixadas/prova_exercicio-4.py
@@ -0,0 +1,11 @@
+﻿# Analise o código abaixo e assinale a resposta correta para a seguinte pergunta: Quantas vezes a linha 6 será executada?
+
+x = 2
+cont = 0
+while x >= 0:
+    
+    y = 0
+    while y >= 4:
+        #comando qualquer
+        y = y + 1
+    x = x - 1
---
+++ semana-7/Teste -Repetições Encaixadas/prova_exercicio-6.py
@@ -0,0 +1,11 @@
+﻿# Observe o programa abaixo:
+# O que precisamos colocar na linha 5 para que o programa imprima "1 2 3 4 5 6 7 8 9"?
+
+i = 0
+while i < 3:
+  j = 0
+  while j < 3:
+    #expressão
+    print(3 * i + j + 1, end=" ")
+    j = j + 1
+  i = i + 1
