9f07e726241677223c35740a16e153202a110bfa
Daniel <daniel.rneto@gmail.com>
2022-09-29 11:39:10 -0300

    Exercicio repeticao encaixada calculando fatorial

--- semana-7/01-Exercicio Resolvido - Repetições Encaixadas/exercicio.py
+++ semana-7/01-Exercicio Resolvido - Repetições Encaixadas/exercicio.py
@@ -2,3 +2,33 @@
 # (Ler uma sequência de números digitados pelo usuário e,
 # para cada número digitado, informar o seu fatorial)
 
+def solucao_propria ():
+    aux = 1
+    numero = int(input("Digite um número inteiro ou 0 para sair: "))
+    while (numero > 0):
+        if (numero == 0):
+            numero = 1
+        else:
+            while (numero != 0):
+                
+                aux = aux * numero
+                #print (numero)
+                numero = numero - 1
+                #print (aux)
+                
+        print (aux)
+        numero = int(input("Digite um número inteiro ou 0 para sair: "))
+
+    #################################
+    # Solução do professor
+    #################################
+    
+n = int(input("Digite um número inteiro: "))
+while (n >= 0):
+    fatorial = 1
+    while (n > 1):
+        fatorial = fatorial * n
+        n = n - 1
+
+    print (fatorial)
+    n = int(input("Digite um número inteiro: "))
