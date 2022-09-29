46146c624864d73337a4fff5ab6ed731d95bc233
Daniel <daniel.rneto@gmail.com>
2022-09-29 14:15:15 -0300

    Enunciado do exercicio

---
+++ semana-7/Lista de exercicios - 5/retangulo-01.py
@@ -0,0 +1,13 @@
+﻿# Exercício 1
+# Escreva um programa que recebe como entradas (utilize a função input) 
+# dois números inteiros correspondentes à largura e à altura de um retângulo, 
+# respectivamente. O programa deve imprimir, usando repetições encaixadas 
+# (laços aninhados), uma cadeia de caracteres que represente o retângulo 
+# informado com caracteres '#' na saída.
+
+# Exemplo:
+# digite a largura: 10
+# digite a altura: 3
+# ##########
+# ##########
+# ##########
