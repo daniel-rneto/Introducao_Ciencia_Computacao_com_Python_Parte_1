abbfcd007c2a683b60862b3bd78fb06f8c912960
Daniel <daniel.rneto@gmail.com>
2022-09-30 13:46:52 -0300

    Logica adicionada

--- semana-7/Exercicios adicionais (opcionais)/primos.py
+++ semana-7/Exercicios adicionais (opcionais)/primos.py
@@ -11,3 +11,29 @@
 # >>>n_primos(121)
 # 30
 
+def ePrimo (x):
+    fator = 2
+    if (x == 2):
+        return True
+    
+    while ((x % fator != 0) and fator <= x/2):
+        fator =  fator + 1
+    
+    if (x % fator == 0):
+        return False
+    else:
+        return True
+
+def n_primos (x):
+    totalNumerosPrimos = 0
+    contador = 2
+    while (contador <= x):
+        if (ePrimo(contador)):
+            totalNumerosPrimos = totalNumerosPrimos + 1
+        contador = contador + 1
+
+    return totalNumerosPrimos
+
+print (n_primos(2))
+print (n_primos(4))
+print (n_primos(121))
