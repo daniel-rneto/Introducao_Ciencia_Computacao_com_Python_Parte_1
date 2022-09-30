0b9b3a758eb5749ca8e20537f651c9fca54d8b26
Daniel <daniel.rneto@gmail.com>
2022-09-30 09:59:26 -0300

    Adicionado o enunciado dos exercicios

---
+++ semana-7/Exercicios adicionais (opcionais)/primos.py
@@ -0,0 +1,13 @@
+﻿# Exercício 1 - Primos
+# Escreva a função n_primos que recebe como argumento um número
+# inteiro maior ou igual a 2 como parâmetro e devolve a quantidade 
+# de números primos que existem entre 2 e n (incluindo 2 e, se for o caso, n).
+
+# Exemplo:
+# >>>n_primos(2)
+# 1
+# >>>n_primos(4)
+# 2
+# >>>n_primos(121)
+# 30
+
