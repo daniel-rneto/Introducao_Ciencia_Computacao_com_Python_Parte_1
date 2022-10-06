7e62ba22ed8a33d377823264b183719ac63721fd
Daniel <daniel.rneto@gmail.com>
2022-10-06 09:29:24 -0300

    Adicionado logica solicitando numeros

--- semana-8/imprime_ordem_inversa.py
+++ semana-8/imprime_ordem_inversa.py
@@ -2,3 +2,14 @@
 # Lê números do teclado. 
 # 0 sai das perguntas e imprime a lista na ordem inversa
 
+lista_numero = []
+
+#numero = int(input("Digite um número OU 0 para sair: "))
+numero = 1
+while (numero != 0):
+    numero = int(input("Digite um número OU 0 para sair: "))
+    if (numero != 0):
+        lista_numero.append(numero)
+    
+
+print (lista_numero)
