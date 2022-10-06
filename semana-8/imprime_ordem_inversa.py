23a7daa0c97e32231698a05913899d6a475529f9
Daniel <daniel.rneto@gmail.com>
2022-10-06 10:51:08 -0300

    Adicionado logica para inverter a lista

--- semana-8/imprime_ordem_inversa.py
+++ semana-8/imprime_ordem_inversa.py
@@ -2,14 +2,25 @@
 # Lê números do teclado. 
 # 0 sai das perguntas e imprime a lista na ordem inversa
 
-lista_numero = []
+def main ():
+    lista_numero = []
 
-#numero = int(input("Digite um número OU 0 para sair: "))
-numero = 1
-while (numero != 0):
-    numero = int(input("Digite um número OU 0 para sair: "))
-    if (numero != 0):
-        lista_numero.append(numero)
-    
+    numero = 1
 
-print (lista_numero)
+    while (numero != 0):
+        numero = int(input("Digite um número OU 0 para sair: "))
+        if (numero != 0):
+            lista_numero.append(numero)
+
+    print (inverte_lista(lista_numero))
+
+def inverte_lista (lista):
+    contador = (len(lista) - 1)
+    lista_invertida = []
+    while (contador >= 0):
+        lista_invertida.append(lista[contador])
+        contador = contador - 1
+    return lista_invertida
+
+main()
+
