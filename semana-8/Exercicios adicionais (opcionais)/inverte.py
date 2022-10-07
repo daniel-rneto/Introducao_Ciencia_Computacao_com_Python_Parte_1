faa94ed16287a74e81e331f618b948eea85370b0
Daniel <daniel.rneto@gmail.com>
2022-10-07 10:54:13 -0300

    Adicionada logica do exercicio.

--- semana-8/Exercicios adicionais (opcionais)/inverte.py
+++ semana-8/Exercicios adicionais (opcionais)/inverte.py
@@ -15,3 +15,24 @@
 # 7
 # 1
 
+def main ():
+    lista_numero = []
+
+    numero = 1
+
+    while (numero != 0):
+        numero = int(input("Digite um número OU 0 para sair: "))
+        if (numero != 0):
+            lista_numero.append(numero)
+
+    print (inverte_lista(lista_numero))
+
+def inverte_lista (lista):
+    contador = (len(lista) - 1)
+    lista_invertida = []
+    while (contador >= 0):
+        lista_invertida.append(lista[contador])
+        contador = contador - 1
+    return lista_invertida
+
+main()
