5417b72dc36b59fd1a9a0de3e38f2452da33af2e
Daniel <daniel.rneto@gmail.com>
2022-10-07 10:29:52 -0300

    Criado o arquivo e adicionado enunciado do exercicio

---
+++ semana-8/Exercicios adicionais (opcionais)/inverte.py
@@ -0,0 +1,17 @@
+﻿# Exercício 2 - Invertendo sequência
+# Como pedido na primeira video-aula desta semana, escreva um programa 
+# que recebe uma sequência de números inteiros e imprima todos os valores 
+# em ordem inversa. A sequência sempre termina pelo número 0. Note que 0 (ZERO) 
+# não deve fazer parte da sequência.
+
+# Exemplo:
+
+# Digite um número: 1
+# Digite um número: 7
+# Digite um número: 4
+# Digite um número: 0
+
+# 4
+# 7
+# 1
+
