f18aabc609b384fef133b7ba3769cb6fef381e2f
Daniel <daniel.rneto@gmail.com>
2022-10-07 09:42:15 -0300

    Logica para remover os numeros duplicados

---
+++ semana-8/Lista de exercicios - 6/remove_repetidos.py
@@ -0,0 +1,41 @@
+﻿# Exercício 1 - Removendo elementos repetidos
+# Escreva a função remove_repetidos que recebe como parâmetro 
+# uma lista com números inteiros, verifica se tal lista possui 
+# elementos repetidos e os remove. A função deve devolver uma lista 
+# correspondente à primeira lista, sem elementos repetidos. 
+# A lista devolvida deve estar ordenada.
+
+# Dica: Você pode usar lista.sort() ou sorted(lista). Qual a diferença?
+
+# Exemplo: 
+# >>>lista = [2, 4, 2, 2, 3, 3, 1]
+# >>>remove_repetidos(lista)
+# [1, 2, 3, 4]
+# >>>remove_repetidos([1, 2, 3, 3, 3, 4])
+# [1, 2, 3, 4]
+
+def remove_repetidos(lista):
+    lista.sort()
+    contadorExterno = 0
+    while (contadorExterno < len(lista)):
+        contadorInterno = contadorExterno
+        while (contadorInterno < len(lista)):
+            if (contadorInterno != contadorExterno):
+                if (lista[contadorExterno] == lista[contadorInterno]):
+                    del lista[contadorInterno:(contadorInterno + 1)]
+                    contadorInterno = contadorInterno - 1
+            contadorInterno = contadorInterno + 1
+        
+        contadorExterno = contadorExterno + 1
+        #contadorInterno = contadorExterno
+
+    return lista
+
+
+lista = [3,2,4,5,7,8,5,3,1,5,7,8,9]
+
+#print (lista)
+
+#print (remove_repetidos(lista))
+
+print (remove_repetidos(lista))
