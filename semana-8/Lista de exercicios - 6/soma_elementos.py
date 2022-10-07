43daf14a126af3fffc165ae5de5b9f816293efee
Daniel <daniel.rneto@gmail.com>
2022-10-07 10:25:06 -0300

    Adicionada logica na função

--- semana-8/Lista de exercicios - 6/soma_elementos.py
+++ semana-8/Lista de exercicios - 6/soma_elementos.py
@@ -3,3 +3,18 @@
 # lista com números inteiros e devolve um número inteiro correspondente 
 # à soma dos elementos da lista recebida.
 
+def soma_elementos(lista):
+    soma = 0
+    contador = 0
+    while (contador < len(lista)):
+        soma = soma + lista[contador]
+        contador = contador + 1
+    return soma
+
+lista = [3,2,4,5,7,8,5,3,1,5,7,8,9]
+
+#print (lista)
+
+#print (remove_repetidos(lista))
+
+print (soma_elementos(lista))
