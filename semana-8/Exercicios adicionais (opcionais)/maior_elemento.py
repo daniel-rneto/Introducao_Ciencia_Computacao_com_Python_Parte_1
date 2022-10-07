a9f293174c4a309e4799b8979f1b6bce0634a059
Daniel <daniel.rneto@gmail.com>
2022-10-07 13:05:15 -0300

    Adicionado nova lógica para retornar o maior elemento da lista

--- semana-8/Exercicios adicionais (opcionais)/maior_elemento.py
+++ semana-8/Exercicios adicionais (opcionais)/maior_elemento.py
@@ -6,16 +6,23 @@
 def main():
 
     lista = [3,2,4,5,7,88,5,3,1,5,7,8,9]
+    #lista = [-90, -27, -12]
     print (maior_elemento(lista))
 
-def maior_elemento(lista):
+# A função abaixo não resolve para lista com números negativos
+def maior_elemento2(lista):
     contador = 0
-    maior = 0
+    #maior = 0
     while (contador < len(lista)):
         if (lista[contador] > maior):
             maior = lista[contador]
         contador = contador + 1
     return maior
 
+def maior_elemento(lista):
+    lista.sort()
+    tamanho = len(lista)
+
+    return lista[tamanho - 1]
 
 main()
